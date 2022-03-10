ee679b9af97eaa16de76b7b14851ae45705ea65c
Aaron Wirth <awirth@gomodus.com>
2022-03-10 08:54:21 -0600

    lesson 2 done

---
+++ javascript/2_loops/README.md
@@ -0,0 +1,15 @@
+# README
+
+Loops
+
+## Objectives
+1. Inside of the scripts.js file there are 4 loop exercises
+2. For extra fun you can write css to alternate the colors of the list elements
+
+## Expected Visual Output 
+![alt text](./readme/output.PNG)
+
+## Some Resources
+- w3schools forEach - https://www.w3schools.com/jsref/jsref_foreach.asp
+- unions, intersections and differences - https://medium.com/@alvaro.saburido/set-theory-for-arrays-in-es6-eb2f20a61848
+- nth child - https://www.w3schools.com/cssref/sel_nth-child.asp
---
+++ javascript/2_loops/scripts.js
@@ -0,0 +1,30 @@
+
+const arrayOfNumbers = [5, 10, 9, 6, 4, 14, 7, 11, 3];
+const arrayOfStrings = ["apple", "banana", "pear", "orange", "strawberry", "raspberry", "blueberry", "grape", "potato", "pea", "cantelope", "watermelon", "dragonfruit"];
+
+//#1 for loop
+//create an <ul> in <div class="for-loop-output"></div> that shows a reverse of the "arrayOfNumbers"
+//example: 3 -> 11 -> 7 -> etc
+
+
+//#2 foreach
+//create a <ul> in <div class='for-each-output"></div>
+//each <li> should be combination of the text from arrayOfStrings + the element at the same index in "arrayOfNumbers". If there isn't a value at index then show "n/a"
+//EXAMPLE: "apple+12" (both of those items are at index 0)
+//EXAMPLE: "dragonfruit+n/a"
+
+
+//#3 nested for OR foreach loops
+//create a <ul> in <div class='for-each-output"></div>
+//loop through "arrayofNumbers" and find the first word in "arrayOfStrings" that has a word length that equals the number. if nothing is find return "n/a"
+//example: 5 = "apple".
+
+
+//#4 create a union
+//only using "forEach" loops (and other fundamentals like if statements) and the the array below "anotherArrayOfNumbers" create a union with "arrayOfNumbers"
+//a union is a combination of arrays that will result in a single array that shows the unique terms from both arrays
+//print the result array to  <div class="union-output"></div>
+//example: ["a", "b", "c"] & ["d", "c", "f", "a"] will create ["c", "a"]
+
+const anotherArrayOfNumbers = [12, 10, 1, 4, 6, 11, 3, 2, 13, 14]
+
